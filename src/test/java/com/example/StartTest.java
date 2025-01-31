1dd89706da16aacca066bc476501ab4abf789776
polinabratchikova <polina-bratchikova@bk.ru>
2025-01-31 21:19:10 +0300

    edit StartTest.java

--- src/test/java/com/example/StartTest.java
+++ src/test/java/com/example/StartTest.java
@@ -2,12 +2,17 @@ package com.example;
 
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+import java.util.List;
 
 public class StartTest {
     public static void main(String[] args) {
         TestListenerAdapter tla = new TestListenerAdapter();
         TestNG testng = new TestNG();
-        testng.setTestClasses(new Class[] { SFTPClientTest.class });
+        List<String> suites = Lists.newArrayList();
+        suites.add("src/test/resources/testng.xml");
+        testng.setTestSuites(suites);
         testng.addListener(tla);
         testng.run();
     }
